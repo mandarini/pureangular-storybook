586c4b396860fabb058acb038db553528abe67a6
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-03-31 16:08:33 +0300

    plain angular app with extra styles asset and storybook

---
+++ src/app/app.component.stories.ts
@@ -0,0 +1,23 @@
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { AppComponent } from './app.component';
+
+export default {
+  title: 'AppComponent',
+  component: AppComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [],
+    }),
+  ],
+} as Meta<AppComponent>;
+
+const Template: Story<AppComponent> = (args: AppComponent) => ({
+  props: args,
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  name: 'katerina',
+  age: 33,
+  happy: true,
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-storybook';
+  @Input() name = 'katerina';
+  @Input() age = 33;
+  @Input() happy = true;
 }
